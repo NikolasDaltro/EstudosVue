1deb6d579dd1a468dc96e7e5e42c4d89635b5025
Nikolas Daltro <nikolasdsantos@gmail.com>
2023-11-18 20:46:55 -0300

    atualização

--- desafios/desafio 04/app.js
+++ desafios/desafio 04/app.js
@@ -2,6 +2,14 @@ new Vue({
 	el: '#desafio',
 	data: {
 		classe1:'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
+		cor5: '',
+		estilo5:{
+			width: '100px',
+			height: '100px'
+		}
 		
 	},
 	methods: {
@@ -15,6 +23,13 @@ new Vue({
 		},
 		iniciarProgresso() {
 
+		},
+		setPerigo(event){
+			if(event.target.value == "true"){
+				this.perigo = true
+			} else if(event.target.value == "false"){
+				this.perigo = false
+			}
 		}
 	}
 })
